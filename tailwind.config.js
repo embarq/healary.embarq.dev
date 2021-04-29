ef282c2156067dd88dc4791fba87fede55379207
embarq <embarq.dev@gmail.com>
2021-04-29 12:53:20 +0300

    use default theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,17 @@
+const defaultConfig = require('tailwindcss/defaultConfig')
+
 module.exports = {
   purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       colors: {
+        ...defaultConfig.theme.colors,
         lightblue: '#458FF6'
       },
     },
     boxShadow: {
+      ...defaultConfig.theme.boxShadow,
+      'lg': '0 10px 15px -3px rgba(229, 233, 246, 0.4), 0 4px 6px -2px rgba(229, 233, 246, 0.2)',
       'xl': '10px 40px 50px rgba(229, 233, 246, 0.4)'
     }
   },
